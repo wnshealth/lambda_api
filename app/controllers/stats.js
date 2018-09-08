722959acf40204f023028e166a8ec2b23aa48d65
Robby Fairclough <robby.fairclough@gmail.com>
2018-09-07 17:34:13 -0700

    scan

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -4,28 +4,40 @@ const moment = require( 'moment' );
 
 var dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+const SOURCE_MAP = {
+  '8caf6d3b-1379-46a2-b931-f5a669cd87a0': 'Test',
+  '91017dd5-e82a-4983-b6f9-b158f9c0106d': 'Zeeto',
+  'f9fbf52d-f0fe-4adf-8ff6-34d67c4685d9': 'Fluent'
+};
+
 class Stats {
   constructor() {}
 
   dynamoDbQuery( req, res, next ) {
     var params = {
-      ExpressionAttributeValues: {
-        ':d': { S: moment().format( 'YYYY-MM-DD' ) },
-       },
-      KeyConditionExpression: 'created_at = :d',
       TableName: 'wns.leads'
     };
 
-    dynamoDb.query( params, ( err, data ) => {
-      if ( err ) return req.data.stats = err;
-      data.Items.forEach( ( el, i, arr ) => {} );
-      req.data.stats = data.Items;
+    dynamoDb.scan( params, ( err, data ) => {
+      if ( err ) { req.data.stats = err; return next(); };
+      var date = req.query.date || moment().format( 'YYYY-MM-DD' );
+      var stats = { created_at: date };
+      var dateFilter = _.filter( data.Items, ( item ) => {
+        return item.created_at === date;
+      });
+      dateFilter.forEach( ( item ) => {
+        var source = SOURCE_MAP[ item.source_id ];
+        stats[ source ] = stats[ source ] ? ++stats[ source ] : 1;
+      });
+      stats.total_leads = dateFilter.length;
+      req.data.stats = stats;
+      next();
     });
   }
 }
 
-module.exports = ( leads => {
+module.exports = ( stats => {
   return {
-    dynamoDbQuery: leads.dynamoDbQuery.bind( leads )
+    dynamoDbQuery: stats.dynamoDbQuery.bind( stats )
   };
 })( new Stats() );
